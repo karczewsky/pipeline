203803ea254defb84a69bd796411b98ac6449b97
wolpear <wolpear@users.noreply.github.com>
2018-11-22 22:40:59 +0100

    Signals implementation

--- main.c
+++ main.c
@@ -4,9 +4,73 @@
 #include <fcntl.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 
+volatile int *pids;
+
+void propagate_signal(int sig) {
+    for(int i=0; i<3; i++) {
+        if(pids[i] != getpid()) {
+            union sigval s;
+            s.sival_int = sig;
+            printf("I try to send to %d signal: %d\n", pids[i], sig);
+            sigqueue(pids[i], SIGUSR2, s);
+        }
+    }
+    fflush(stdout);
+}
+
+void sighandler(int sig, siginfo_t *info, void *context) {
+    if(sig == SIGUSR2) {
+        sig = info->si_value.sival_int;
+    } else {
+        propagate_signal(sig);
+    }
+
+    switch (sig) {
+        case SIGUSR1:
+            printf("%d Ending\n", getpid());
+            exit(0);
+        case SIGINT:
+            printf("%d Pausing\n", getpid());
+            fflush(stdout);
+
+            sigset_t set;
+            sigfillset(&set);
+            sigdelset(&set, SIGCONT);
+            sigdelset(&set, SIGUSR2);
+            sigsuspend(&set);
+
+            break;
+        case SIGCONT:
+            printf("%d Resuming\n", getpid());
+            fflush(stdout);
+            break;
+        default:
+            break;
+    }
+}
 
 int main() {
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = sighandler;
+    sigemptyset(&sa.sa_mask);
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGCONT, &sa, NULL);
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    pids = mmap(NULL, 3*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    if(!pids) {
+        perror("Blad tworzac mmap");
+        return 1;
+    }
+
     int fd1;
     char *fifoFile = "/tmp/projekt";
     mkfifo(fifoFile, 0666);
@@ -17,8 +81,8 @@ int main() {
         return 1;
     }
 
-
     if (!fork()) {
+        pids[0] = getpid();
         char x;
         fd1 = open(fifoFile, O_WRONLY);
         while (read(STDIN_FILENO, &x, 1)) {
@@ -29,6 +93,7 @@ int main() {
     }
 
     if (!fork()) {
+        pids[1] = getpid();
         close(fd2[0]);
 
         char x;
@@ -47,11 +112,12 @@ int main() {
     }
 
     if(!fork()) {
+        pids[2] = getpid();
         close(fd2[1]);
         char x;
-        fprintf(stdout, "Proces 3 odebrał:\n");
+//        fprintf(stdout, "Proces 3 odebrał:\n");
         while (read(fd2[0], &x, 1)) {
-            write(STDOUT_FILENO, &x, sizeof(x));
+//            write(STDOUT_FILENO, &x, sizeof(x));
         }
         close(fd2[0]);
         return 0;
