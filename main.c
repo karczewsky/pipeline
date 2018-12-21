e47ff4f8376757dcea35b68ae866ec94e70d7998
wolpear <wolpear@users.noreply.github.com>
2018-12-21 10:10:14 +0100

    Add mutexes for read/write

--- main.c
+++ main.c
@@ -15,15 +15,43 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <pthread.h>
+
+typedef struct
+{
+    int bytes_left;
+    pthread_mutex_t mutex_write;
+    pthread_mutex_t mutex_read;
+} shared_data;
+
+static shared_data* data = NULL;
 
 volatile int *pids;
+
+
+void initialise_shared()
+{
+    int prot = PROT_READ | PROT_WRITE;
+    int flags = MAP_SHARED | MAP_ANONYMOUS;
+    data = mmap(NULL, sizeof(shared_data), prot, flags, -1, 0);
+
+    data->bytes_left = 0;
+
+    pthread_mutexattr_t attr_read; pthread_mutexattr_t attr_write;
+    pthread_mutexattr_init(&attr_read); pthread_mutexattr_init(&attr_write);
+    pthread_mutexattr_setpshared(&attr_read, PTHREAD_PROCESS_SHARED);
+    pthread_mutexattr_setpshared(&attr_write, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&data->mutex_read, &attr_read);
+    pthread_mutex_init(&data->mutex_write, &attr_write);
+    pthread_mutex_lock(&data->mutex_read);
+}
+
 
 void propagate_signal(int sig) {
     for(int i=0; i<3; i++) {
         if(pids[i] != getpid()) {
             union sigval s;
             s.sival_int = sig;
-//            printf("I try to send to %d signal: %d\n", pids[i], sig);
             sigqueue(pids[i], SIGUSR2, s);
         }
     }
@@ -31,18 +59,18 @@ void propagate_signal(int sig) {
 }
 
 void sighandler(int sig, siginfo_t *info, void *context) {
-    if(sig == SIGUSR2) {
+    if(sig == SIGUSR2)
         sig = info->si_value.sival_int;
-    } else {
+    else
         propagate_signal(sig);
-    }
+
 
     switch (sig) {
+        // END
         case SIGUSR1:
-//            printf("%d Ending\n", getpid());
             exit(0);
+        // PAUSE
         case SIGINT:
-//            printf("%d Pausing\n", getpid());
             fflush(stdout);
 
             sigset_t set;
@@ -53,8 +81,8 @@ void sighandler(int sig, siginfo_t *info, void *context) {
             sigsuspend(&set);
 
             break;
+        // RESUME
         case SIGCONT:
-//            printf("%d Resuming\n", getpid());
             fflush(stdout);
             break;
         default:
@@ -81,17 +109,17 @@ int main() {
         return 1;
     }
 
-    // Mechanizm komunikacji 1: kolejka FIFO
+
+    // Mechanizm komunikacji 1: Kolejka FIFO
     int fd1;
-    char *fifoFile = "/tmp/projekt";
+    char *fifoFile = "./projekt_fifo";
     mkfifo(fifoFile, 0666);
 
-    // Mechanizm komunikacji 2: plik
-    int fd2[2];
-    if(pipe(fd2)) {
-        fprintf(stderr, "Error creating pipe");
-        return 1;
-    }
+    // Mechanizm komunikacji 2: Plik
+    FILE *fd2;
+    char *mutexFile = "./projekt_mutex";
+    initialise_shared();
+
 
     // Proces 1: czyta dane ze standardowego strumienia wejsciowego i przekazuje je w niezmienionej formie do procesu 2
     if (!fork()) {
@@ -99,9 +127,10 @@ int main() {
         fd1 = open(fifoFile, O_WRONLY);
         char x;
         if(isatty(fileno(stdin))) {
+            printf("Podaj ciag znakow do odczytania przez proces 1: ");
             char buff[256];
             int loop = 1;
-            do {
+            while (loop) {
                 fgets(buff, sizeof(buff), stdin);
                 for (int i = 0; i < 256; i++) {
                     if (buff[i] == '\n') {
@@ -111,7 +140,7 @@ int main() {
                         write(fd1, &buff[i], sizeof(buff[i]));
                     }
                 }
-            } while (loop);
+            }
         } else {
             while (read(STDIN_FILENO, &x, 1)) {
                 write(fd1, &x, sizeof(x));
@@ -122,23 +151,35 @@ int main() {
         return 0;
     }
 
-    // Proces 2: pobiera dane przeslane przez proces 1. Oblicza ilosc odczytanych bajtow, wyswietla ja na standardowym strumiwniu diagnostycznym,
-    // a nastepnie przekazuje odebrane dane mechanizmem komunikacji do procesu 3
+    // Proces 2: pobiera dane przeslane przez proces 1.
+    //           Oblicza ilosc odczytanych bajtow, wyswietla ja na standardowym strumieniu diagnostycznym,
+    //           a nastepnie przekazuje odebrane dane mechanizmem komunikacji do procesu 3
     if (!fork()) {
         pids[1] = getpid();
-        close(fd2[0]);
 
         fd1 = open(fifoFile, O_RDONLY);
 
         char x;
         unsigned long long count = 0;
-        while (read(fd1, &x, 1)) {
-            write(fd2[1], &x, sizeof(x));
-            count++;
+        fd2 = fopen(mutexFile, "w");
+        if (fd2 == NULL) {
+            fprintf(stderr, "PROC 2: error opening communication file");
+            return 1;
         }
+
+        while (read(fd1, &x, 1)) {
+            pthread_mutex_lock(&data->mutex_write);
+
+            fprintf(fd2, "%c", x);
+            fflush(fd2);
+            data->bytes_left++;
+            count++;
+            fprintf(stdout, "PROC 2 | Bytes left: %d\n", data->bytes_left);
+            pthread_mutex_unlock(&data->mutex_read);
+        }
+        pthread_mutex_unlock(&data->mutex_read);
         unlink(fifoFile);
-        close(fd2[1]);
-        fprintf(stderr, "Proces 2 odczytał: %lld bytes\n", count);
+        fprintf(stderr, "\nProces 2 odczytał: %lld bytes\n", count);
         return 0;
     }
 
@@ -146,14 +187,41 @@ int main() {
     // Proces 3: pobiera dane wyprodukowane przez proces 2 i umieszcza je w standardowym strumieniu wyjsciowym.
     if(!fork()) {
         pids[2] = getpid();
-        close(fd2[1]);
+
+        fd2 = fopen(mutexFile, "r");
+        if (fd2 == NULL) {
+            fprintf(stderr, "PROC 3: error opening communication file");
+            return 1;
+        }
 
         char x;
-        fprintf(stdout, "Proces 3 odebrał:\n");
-        while (read(fd2[0], &x, 1)) {
-            fprintf(stdout, "%c", x);
+        int loop = 1;
+        int first_iter = 1;
+        while (loop)
+        {
+            pthread_mutex_lock(&data->mutex_read);
+            if (first_iter) {
+                first_iter = 0;
+                fprintf(stdout, "Proces 3 odebrał:\n");
+            }
+
+
+            if (data->bytes_left > 0) {
+                x = (char)fgetc(fd2);
+//                printf("%c", x);
+
+                fflush(stdout);
+                data->bytes_left--;
+                fprintf(stdout, "PROC 3 | Bytes left: %d\n", data->bytes_left);
+                pthread_mutex_unlock(&data->mutex_write);
+            } else {
+                printf("Koniec %d", data->bytes_left);
+                fflush(stdout);
+                loop = 0;
+            }
         }
-        close(fd2[0]);
+
+        unlink(mutexFile);
         return 0;
     }
 
